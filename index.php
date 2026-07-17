79a5fa2de997d01fa0a3365e040ca59ad2557c0d
eaboybi <boybilis@gmail.com>
2026-07-17 15:31:49 +0800

    deployment update cards

--- index.php
+++ index.php
@@ -121,7 +121,7 @@ function notification_badge($count)
 <title>Cooperative Dashboard</title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 <link rel="stylesheet" href="assets/css/mobile.css">
-<link rel="stylesheet" href="assets/css/theme.css">
+<link rel="stylesheet" href="assets/css/theme.css?v=20260717">
 <style>
 .notification-badge {
     position: absolute;
