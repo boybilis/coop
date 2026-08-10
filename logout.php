8e691aec1d4b6cc87ab0b4709acbffa38e15d0f3
boybilis <josepharamil@gmail.com>
2026-08-10 22:55:37 +0800

    update harden authentication

--- logout.php
+++ logout.php
@@ -1,9 +1,23 @@
 <?php
 include 'auth.php';
 
-session_unset();
+$_SESSION = [];
+
+if (ini_get('session.use_cookies')) {
+    $cookie = session_get_cookie_params();
+    setcookie(session_name(), '', [
+        'expires' => time() - 42000,
+        'path' => $cookie['path'],
+        'domain' => $cookie['domain'],
+        'secure' => $cookie['secure'],
+        'httponly' => $cookie['httponly'],
+        'samesite' => $cookie['samesite'] ?? 'Lax',
+    ]);
+}
+
 session_destroy();
 
-header("Location: login.php");
+header('Clear-Site-Data: "cache"');
+header('Location: login.php?logged_out=1');
 exit;
 
